477ee7340d179805ef5ad71cc543379bb53d1d0d
myindiankisan <multisuperapp@gmail.com>
2026-04-09 10:51:10 +0530

    1st committ

---
+++ src/main/java/com/msa/booking_paymenet/BookingPaymenetApplication.java
@@ -0,0 +1,13 @@
+package com.msa.booking_paymenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingPaymenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingPaymenetApplication.class, args);
+	}
+
+}
